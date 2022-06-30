0cc2953692a61694a423aad201caa433b97c70a9
babken.ashot2000 <babken.ashot2000@gmail.com>
2022-07-01 00:59:14 +0400

    Add a few math operations

--- HomeWork-1/Program.cs
+++ HomeWork-1/Program.cs
@@ -11,9 +11,22 @@ namespace HomeWork_1
         static void Main(string[] args)
         {
             int num1 = 5;
-            int num2 = 12;
+            string num2 = Console.ReadLine();
+            int num3 = 12;
 
-            Console.Write(num1 + num2);
+            Console.Write(num1 + num2); // Concatenation operation
+            Console.WriteLine(num1 + num3); // Simple Sum and endline
+            Console.WriteLine(num1*num3); // mul operetion
+
+            char simbol1 = 'a';
+
+            Console.Write(simbol1+num2);//Concatenation opr
+
+            byte num4 = 4;
+
+            Console.WriteLine(num3/num4); // Return result after operetion devided
+            //I could write something like these, but nothing else came to mind)
+            Console.ReadKey();
         }
     }
 }
