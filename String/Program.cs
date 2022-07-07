b7fd99ae10897a642007fcfaf56a15f5fe3ab28a
babken.ashot2000 <babken.ashot2000@gmail.com>
2022-07-07 23:20:09 +0400

    Leaner solution string task

--- String/Program.cs
+++ String/Program.cs
@@ -8,39 +8,37 @@ namespace String
 {
     class Program
     {
+        // Finde most often symbol and count in string. Algrorithm asimptot analyze O(n).
         static void Main(string[] args)
         {
             string Data = Console.ReadLine();
-            int count = 1;
-            int max = -1;
-            char symbol = ' ';
 
+            int[] asciiArr = new int[256];
+            int max_counter = 0;
+            char value = ' ';
 
             for (int i = 0; i < Data.Length; ++i)
             {
-                count ^= count;  // xor 
-                for (int j = i; j < Data.Length - 1; ++j)
-                {
-                   
-                    if (Data[i] == Data[j + 1])
-                    {
-                        count++;
-                    }
-
-                }
-                if (count > max)
-                {
-                    
-                    symbol = Data[i];
-                }
-
+                ++asciiArr[Data[i]];   // O(n)
             }
 
-            Console.WriteLine("Symbol: " + symbol);
-
-
+            for (int i = 0; i < Data.Length; ++i)
+            {
+                if (asciiArr[Data[i]] > max_counter)
+                {
+                    max_counter = asciiArr[Data[i]];  //O(n)  in general we get O(2n) => O(n)
+                    value = Data[i];
+                }
+            }
+            Console.WriteLine("Symbol: " + value + " Count: " + max_counter);
 
             Console.ReadKey();
         }
+
+
+
+
+
+
     }
 }
